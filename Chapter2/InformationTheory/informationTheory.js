8e7021cd31e0db4507a4e15556555b038fad2ba6
drozdzl <lukasz.drozdz83@gmail.com>
2026-06-22 19:32:44 +0200

    it

--- Chapter2/InformationTheory/informationTheory.js
+++ Chapter2/InformationTheory/informationTheory.js
@@ -10,3 +10,29 @@ var roznorodnosc10 = function (liczbaStanowUkladu) {
     return Math.log10(liczbaStanowUkladu);
 }
 
+//entropia w ditach
+var entropiaD = function(tabl) {
+    var sum = 0;
+    for (var i = 0; i < tabl.length; i++) {
+        sum += tabl[i] * Math.log10(tabl[i]);
+    }
+    return -sum;
+}
+
+// entropia w bitach
+var entropiaB = function(tabl) {
+    var sum = 0;
+    for (var i = 0; i < tabl.length; i++) {
+        sum += tabl[i] * Math.log2(tabl[i]);
+    }
+    return -sum;
+}
+
+// srednia entropia w bitach
+var sredniaEntropia = function(tabl) {
+    var sum = 0;
+    for (var i = 0; i < tabl.length; i++) {
+        sum += tabl[i] * Math.log2(1 / tabl[i]);
+    }
+    return -sum;
+}
